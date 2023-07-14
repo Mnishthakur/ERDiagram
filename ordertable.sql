246ed8b06b8e3818766848a4a5a711567c819015
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-07-14 19:52:35 +0530

    [Manish Thakur]: Added Order table with ER diagram successfully

--- ordertable.sql
+++ ordertable.sql
@@ -9,3 +9,10 @@ CREATE TABLE Customer (
     state VARCHAR(50),
     zip_code VARCHAR(20)
 );
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2),
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
