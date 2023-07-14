9f56a55c478eb1d5f339dfdbf0ff7025c9c66a41
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-07-14 19:50:53 +0530

    [Manish Thakur]: Added Customer table with er diagram successfully

---
+++ customertable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Customer (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone_number VARCHAR(20),
+    address VARCHAR(100),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    zip_code VARCHAR(20)
+);
